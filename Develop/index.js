0e5a86a6181dca2e55001063b4d51f93ddbaeede
Fausnightm <m.fausnight@lafilm.edu>
2021-07-19 22:21:25 -0400

    added inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+// Install inquirer
+// place inquirer in a variable
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+// Title, description, table of contents, installation, usage, license, contributing, tests, and questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
