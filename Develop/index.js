6977d8ae0349a898a80b76912265d83fec4dc8e0
Fausnightm <m.fausnight@lafilm.edu>
2021-07-23 18:53:06 -0400

    troubleshooting application

--- Develop/index.js
+++ Develop/index.js
@@ -23,8 +23,18 @@ const questions = [
     },
     {
         type: "input",
-        name: "description",
+        name: "Description",
         message: "Please provide your application's description.",
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+        {
+        type: 'checkbox',
+        name: 'built with',
+        message: 'Please select the technologies that your application was built with.',
+        choices: ['HTML', 'CSS', 'SASS', 'JavaScript', 'Node.js', 'Express.js'],
+        default: 0,
+        when: ({ contents }) => { if (contents.indexOf('Built With') > -1) {return true;} else {return false;}},
         validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
@@ -32,28 +42,28 @@ const questions = [
         type: "input",
         name: "Instructions",
         message: "Please provide instructions.",
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
     {
         type: "input",
         name: "Usage",
         message: "How do you use your application?",
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
     {
         type: "input",
         name: "Credits",
         message: "Are there any credits to be provided?",
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
     {
         type: "input",
-        name: "Usage",
-        message: "How do you use your application?",
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        name: "tests",
+        message: "Enter tests:",
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
     {
@@ -61,17 +71,49 @@ const questions = [
         name: "License",
         message: "What license did you use?",
         choices: ['The MIT license', 'The GPL license', 'Apache license', 'N/A'],
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
     {
         type: "input",
-        name: "Github",
+        name: "github",
         message: "What is your Github username?",
-        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "What is your email?",
+        // validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
 
     },
 ];
+
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const userUrl = `https://api.github.com/users/${data.github}`;
+
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file created with success!");
+          });
+        });
+
+    })
 
 // Title, description, table of contents, installation, usage, license, contributing, tests, and questions
 
