fe02632257536a56687690f0c6bbe54a8fdb390b
Fausnightm <m.fausnight@lafilm.edu>
2021-07-23 15:51:50 -0400

    added the parameters of questions

--- Develop/index.js
+++ Develop/index.js
@@ -11,11 +11,66 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is the title of your project?"
+        message: "What is the title of your project?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
     },
     {
-        
-    }
+        type: "input",
+        name: "installation",
+        message: "How will you install your application?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your application's description.",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Instructions",
+        message: "Please provide instructions.",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "How do you use your application?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Credits",
+        message: "Are there any credits to be provided?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "How do you use your application?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "list",
+        name: "License",
+        message: "What license did you use?",
+        choices: ['The MIT license', 'The GPL license', 'Apache license', 'N/A'],
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your Github username?",
+        validate: (value)=>{ if(value){return true} else{return 'I need a valid input to continue'}},
+
+    },
 ];
 
 // Title, description, table of contents, installation, usage, license, contributing, tests, and questions
